e256af1fc5d362a6e727e95acf5d938441533231
danagul <danagul090103@gmail.com>
2022-05-19 00:46:29 +0600

    registration

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const mongoose = require('mongoose')
 const session = require('express-session')
 const axios = require('axios')
+const User = require('./model/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const path = require("path");
+
 const urlencodedParser = bodyParser.urlencoded({
+
     extended: false,
 })
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
-app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/register',
+    err => {
+        if(err) throw err;
+        console.log('connected to MongoDB')
+    });
+
+const app = express()
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
 
 app.use (
     session ({
@@ -18,6 +33,65 @@ app.use (
     })
 
 );
+app.post('/api/register', async (req, res) => {
+    const { username, email, sm, country, pwd: plainTextPassword } = req.body
+
+    if (!username || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid username' })
+    }
+
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    if (plainTextPassword.length < 8) {
+        return res.json({
+            status: 'error',
+            error: 'Password too small. Should be at least 8 characters'
+        })
+    }
+    if(plainTextPassword.search(/[a-z]/i)<0) {
+        return res.json({
+            status: 'error',
+            error: 'Password should contain at least 1 letter'
+        })
+    }
+    if(plainTextPassword.search(/[A-Z]/i)<0) {
+        return res.json({
+            status: 'error',
+            error: 'Password should contain at least 1 capital letter'
+        })
+    }
+    if(plainTextPassword.search(/[!\@\#\$\%\^\&\*\(\)\_\=\+\-\>\<\,\?]/) === -1) {
+        return res.json({
+            status: 'error',
+            error: 'Password should contain at least 1 special character'
+        })
+    }
+
+    const pwd = await bcrypt.hash(plainTextPassword, 10)
+
+    try {
+        const response = await User.create({
+            username,
+            pwd,
+            email,
+            sm,
+            country,
+
+        })
+        console.log('User created successfully: ', response)
+    } catch (error) {
+        if (error.code === 11000) {
+            // duplicate key
+            return res.json({ status: 'error', error: 'Username already in use' })
+        }
+        throw error
+    }
+
+    res.json({ status: 'ok' })
+})
+
 app.get('/usersession', function(req, res, next) {
 
     if (req.session.views) {
