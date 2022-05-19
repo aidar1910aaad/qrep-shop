a936fe296bca2c23806587d2764c462246844ba1
danagul <danagul090103@gmail.com>
2022-05-19 19:47:17 +0600

    register

--- app.js
+++ app.js
@@ -1,166 +1,65 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const axios = require('axios')
-const User = require('./model/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const path = require("path");
+let express = require('express');
+let env = require('dotenv').config()
+let ejs = require('ejs');
+let path = require('path');
+let app = express();
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let session = require('express-session');
 
-const urlencodedParser = bodyParser.urlencoded({
 
-    extended: false,
-})
-const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.2f4sk.mongodb.net/?retryWrites=true&w=majority',
-    err => {
-        if(err) throw err;
-        console.log('connected to MongoDB')
-    });
-
-const app = express()
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.json())
-
-app.use (
-    session ({
-        secret: "fff",
-        resave: true,
-        saveUninitialized: false,
-        cookie: {}
-    })
-
-);
-app.post('/api/register', async (req, res) => {
-    const { username, email, sm, country, pwd: plainTextPassword } = req.body
-
-    if (!username || typeof username !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid username' })
-    }
-
-    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
-        return res.json({ status: 'error', error: 'Invalid password' })
-    }
-
-    if (plainTextPassword.length < 8) {
-        return res.json({
-            status: 'error',
-            error: 'Password too small. Should be at least 8 characters'
-        })
-    }
-    if(plainTextPassword.search(/[a-z]/i)<0) {
-        return res.json({
-            status: 'error',
-            error: 'Password should contain at least 1 letter'
-        })
-    }
-    if(plainTextPassword.search(/[A-Z]/i)<0) {
-        return res.json({
-            status: 'error',
-            error: 'Password should contain at least 1 capital letter'
-        })
-    }
-    if(plainTextPassword.search(/[!\@\#\$\%\^\&\*\(\)\_\=\+\-\>\<\,\?]/) === -1) {
-        return res.json({
-            status: 'error',
-            error: 'Password should contain at least 1 special character'
-        })
-    }
-
-    const pwd = await bcrypt.hash(plainTextPassword, 10)
-
-    try {
-        const response = await User.create({
-            username,
-            pwd,
-            email,
-            sm,
-            country,
-
-        })
-        console.log('User created successfully: ', response)
-    } catch (error) {
-        if (error.code === 11000) {
-            // duplicate key
-            return res.json({ status: 'error', error: 'Username already in use' })
-        }
-        throw error
-    }
-
-    res.json({ status: 'ok' })
-})
-
-app.get('/usersession', function(req, res, next) {
-
-    if (req.session.views) {
-
-        // Increment the number of views.
-        req.session.views++
-
-        // Print the views.
-        res.write('<p> No. of views: '
-            + req.session.views + '</p>')
-        res.end()
+mongoose.connect('mongodb+srv://admin:admin@cluster0.2f4sk.mongodb.net/registration?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.');
     } else {
-        req.session.views = 1
-        res.end(' New session is started')
+        console.log('Error in DB connection : ' + err);
     }
-})
+});
 
 
-app.get('/signin', urlencodedParser, function (
-    request,
-    response
-) {
-    response.sendFile(__dirname + '/public/pages/auth.html')
-})
-app.get('/cart', urlencodedParser, function (
-    request,
-    response
-) {
-    response.sendFile(__dirname + '/public/pages/cart.html')
-})
-app.get('/wishlist', urlencodedParser, function (
-    request,
-    response
-) {
-    response.sendFile(__dirname + '/public/pages/wishlist.html')
-})
-app.get('/hoodies', urlencodedParser, function (
-    request,
-    response
-) {
-    response.sendFile(__dirname + '/public/pages/catalog/hoodies.html')
-})
-app.get('/register', urlencodedParser, function (
-    request,
-    response
-) {
-    response.sendFile(__dirname + '/public/register.html')
-})
-app.post('/register', urlencodedParser, function (
-    request,
-    response
-) {
-    if (!request.body) return response.sendStatus(400)
-    console.log(request.body)
-    response.send(
-        `Email - ${request.body.email} Name - ${request.body.fname} Country - ${request.body.country}`
-    )
-})
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+});
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/public/pages/index.html')
-})
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    cookie:{httpOnly:true}
 
+}));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+let index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
 
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('http://localhost:3000/')
+
+    console.log('Server is started on http://127.0.0.1:'+3000)
 })
 
---
+++ models/user.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+userSchema = new Schema( {
+	
+	unique_id: Number,
+	email: String,
+	username: String,
+	password: String,
+	passwordConf: String
+}),
+User = mongoose.model('User', userSchema);
+
+module.exports = User;
